34932ce14069ddc4a513faeb327490116b151172
劉佳勳 <a917382460@gmail.com>
2024-04-05 13:26:50 +0800

    Updates

--- assets/search-3FYsksMv.js
+++
@@ -1,1 +0,0 @@
-import{D as a,E as r}from"./index-2O7obpK7.js";const s=a("search",{state:()=>({categoryData:""}),actions:{search(t){this.categoryData=t,r.push("/products")},searchOff(){this.categoryData=""}}});export{s};
