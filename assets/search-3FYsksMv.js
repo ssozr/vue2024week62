45bdf2fd029f9408692c27809df9449662ddbf7f
劉佳勳 <a917382460@gmail.com>
2024-04-05 13:18:09 +0800

    Updates

---
+++ assets/search-3FYsksMv.js
@@ -0,0 +1,1 @@
+import{D as a,E as r}from"./index-2O7obpK7.js";const s=a("search",{state:()=>({categoryData:""}),actions:{search(t){this.categoryData=t,r.push("/products")},searchOff(){this.categoryData=""}}});export{s};
--- assets/search-TPWhesJJ.js
+++
@@ -1,1 +0,0 @@
-import{D as a,E as r}from"./index-l_moKGUM.js";const s=a("search",{state:()=>({categoryData:""}),actions:{search(t){this.categoryData=t,r.push("/products")},searchOff(){this.categoryData=""}}});export{s};
